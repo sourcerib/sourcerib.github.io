5ed474122283701c9ee24c5734a0f35e9725981a
Yubion <129749957+Yubion@users.noreply.github.com>
2023-05-30 22:05:23 +0630

    Separated JavaScript Files

---
+++ assets/scripts/home.js
@@ -0,0 +1,31 @@
+
+function humburgur_menu() {
+
+    var humburgur_menu = document.getElementById("humburgur-menu-items");
+    var mainContent = document.getElementById("main")
+    var open = document.getElementById("open-humburgur-menu-items")
+    var close = document.getElementById("close-humburgur-menu-items")
+
+    if (humburgur_menu.style.display === "none") {
+
+        humburgur_menu.style.display = "block";
+
+        mainContent.style.display = "none";
+
+        close.style.display = "block";
+        open.style.display = "none";
+
+    }
+
+    else {
+
+        humburgur_menu.style.display = "none";
+
+        mainContent.style.display = "block"
+
+        close.style.display = "none";
+        open.style.display = "block";
+
+    }
+
+}
